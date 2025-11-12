64b1383fe0a1ac410ff6d025648fdea84b23ef98
Joseph Fredy Marcani Leyva <josephmarcani23@gmail.com>
2025-11-11 22:31:57 -0500

    Definición de esquema de base de datos con relaciones y Soft Deletes

--- database/migrations/2025_11_12_032839_create_notas_table.php
+++ database/migrations/2025_11_12_032839_create_notas_table.php
@@ -6,20 +6,18 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('notas', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('titulo');
+            $table->text('contenido');
+            $table->softDeletes(); // Habilitar eliminaciones lógicas
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('notas');
